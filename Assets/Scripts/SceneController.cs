faa1c9c273fe452214f8b6a5048e3f6754d9b418
astonopuk <ben.hopgood@mail.bcu.ac.uk>
2019-10-08 12:29:30 +0100

    Added Randomization feature for maps. Started production of A*/Djikstra search algorithm.

---
+++ Assets/Scripts/SceneController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneController : MonoBehaviour
+{
+
+    static int[,] GridMap = new int[38,20];
+    static int[,] Blockade = new int[38, 20];
+
+    public Rigidbody Cube;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        for (int z = 0; z <= 19; z++)
+        {
+
+            for (int x = 0; x <= 37; x++)
+            {
+
+                int rIntX = Random.Range(0, 4);
+
+                if (rIntX == 1)
+                {
+
+                    Instantiate(Cube, new Vector3(x, 1, z), Quaternion.identity);
+                    Blockade[x, z] = 1;
+
+                }
+                else
+                {
+
+                    Blockade[x, z] = 0;
+
+                }
+
+            }
+
+        }
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+
+
+    }
+
+}
